908a175775da76612eb1a5bfad5d8e688e3fcfcc
Carson <carsonjamescook@gmail.com>
2020-03-08 12:17:12 -0700

    Working on docstrings

--- acquisition/acquisition_functions.py
+++ acquisition/acquisition_functions.py
@@ -6,14 +6,47 @@ from util.defs import *
 
 
 class AcquisitionFunction(ABC):
+    """ Acquisition functions are used by an acquisition optimizer in order
+    to optimize inputs to a surrogate function.
+
+    Attributes:
+        epsilon (float): Value used in order to avoid divide by zero
+            error. Should be small (<= 1e-9)
+    """
+    def __init__(self, epsilon=1e-11):
+        """ 
+        Args:
+            epsilon (float): Initialization value for epsilon.
+        """
+        self.epsilon = epsilon
+
     @abstractmethod
+<<<<<<< Updated upstream
     def acquire(self, surrogate: object, X: NDArray, f_hat: float) -> NDArray:
+=======
+    def acquire(self, X: NDArray, surrogate: object, f_hat: float) -> NDArray:
+        """ Evaluate the acquisition function.
+        
+        Args:
+            X (NDArray): A sample of values on which to evaluate the surrogate.
+            surrogate (object): The surrogate which models the true objective.
+            f_hat (float): The best predicted value of the surrogate 
+                found so far.
+        
+        Returns:
+            NDArray: One acquisition function evaluation for each row of X
+                (in order). Output will be scalar for 1d X input.
+            
+        """
+>>>>>>> Stashed changes
         pass
 
 
 class ProbabilityOfImprovement(AcquisitionFunction):
+    """ Implementation of the probability of improvement acquisition function.
+    """
     def __init__(self, epsilon: float = 1e-11):
-        self.epsilon = epsilon
+        super(ProbabilityOfImprovement, self).__init__(epsilon)
 
     def acquire(self, surrogate: object, X: NDArray, f_hat: float) -> NDArray:
         mu, std = surrogate.predict(X, return_std=True)
@@ -28,7 +61,7 @@ class ProbabilityOfImprovement(AcquisitionFunction):
 
 class ExpectedImprovement(AcquisitionFunction):
     def __init__(self, epsilon: float = 1e-11):
-        self.epsilon = epsilon
+        super(ExpectedImprovement, self).__init__(epsilon)
 
     def acquire(self, surrogate: object, X: NDArray, f_hat: float) -> NDArray:
         mu, std = surrogate.predict(X, return_std=True)
